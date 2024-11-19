f0a19468db688d29f0467d5e93692c27ee8ba2e2
Bruna Barreto <148925493+itsbrunabarreto@users.noreply.github.com>
2024-11-19 13:18:12 -0300

    Service

---
+++ IFSPStore.Service/Validators/VendaValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using IFSPStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFSPStore.Service.Validators
+{
+    public class VendaValidator : AbstractValidator<Venda>
+    {
+        public VendaValidator()
+        {
+            RuleFor(v => v.ValorTotal)
+                .NotEmpty().WithMessage("Por Favor informe o valor.")
+                .NotNull().WithMessage("Por Favor informe o valor.");
+
+        }
+    }
+
+    public class VendaItemValidator : AbstractValidator<VendaItem>
+    {
+        public VendaItemValidator()
+        {
+            RuleFor(v => v.Quantidade)
+                .NotEmpty().WithMessage("Por Favor informe o quantidade.")
+                .NotNull().WithMessage("Por Favor informe o quantidade.");
+
+        }
+    }
+}
