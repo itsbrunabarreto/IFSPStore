09b7c365aca49105621af50a8e02d718f663e68a
Bruna Barreto <148925493+itsbrunabarreto@users.noreply.github.com>
2024-11-19 13:17:22 -0300

    Repository

---
+++ IFSPStory.Repository/Mapping/UsuarioMap.cs
@@ -0,0 +1,43 @@
+﻿
+using IFSPStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IFSPStory.Repository.Mapping
+{
+    public class UsuarioMap : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.ToTable("Usuario");
+
+            builder.HasKey(prop => prop.Id);
+            
+            builder.Property(prop => prop.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.Property(prop => prop.Senha)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.Property(prop => prop.Login)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.Property(prop => prop.Email)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.Property(prop => prop.DataCadastro)
+                .HasDefaultValue(DateTime.Now);
+
+            builder.Property(prop => prop.DataLogin)
+                .IsRequired();
+
+            builder.Property(prop => prop.Ativo);
+
+
+        }
+    }
+}
