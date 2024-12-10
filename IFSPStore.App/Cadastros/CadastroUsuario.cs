8942487b1c7e9707c6b10a1c5b99ea04fc750abb
itsbrunabarreto <bruna.barreto@aluno.ifsp.edu.br>
2024-12-10 09:04:19 -0300

    Alterações Cadastros

--- IFSPStore.App/Cadastros/CadastroUsuario.cs
+++ IFSPStore.App/Cadastros/CadastroUsuario.cs
@@ -3,15 +3,6 @@ using IFSPStore.App.Models;
 using IFSPStore.Domain.Base;
 using IFSPStore.Domain.Entities;
 using IFSPStore.Service.Validators;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace IFSPStore.App.Cadastros
 {
